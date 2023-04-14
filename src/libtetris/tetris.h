f4743e249127a9c27abee4025c44a986e472a7c0
azard285 <pavlenkopasa382@gmail.com>
2023-04-14 21:13:32 +0700

    created prototypes of functions for creating game objects, #5

---
+++ src/libtetris/tetris.h
@@ -0,0 +1,40 @@
+#pragma once
+
+typedef struct Block
+{
+  int b; // присутствует ли блок на поле
+} Block;
+
+typedef struct Field
+{
+  int width;
+  int height;
+  Block *blocks;
+} Field;
+
+typedef struct Figure
+{
+  int x;
+  int y;
+  int size;
+  Block *blocks;
+} Figure;
+
+typedef struct Figures
+{
+  int count;
+  int size;
+  Block *blocks;
+} Figures;
+
+typedef struct Game
+{
+  Field *field;
+  Figure *figure;
+  Figures *figures;
+} Game;
+
+Field *createField(int widthT, int heightT);
+Figures *createFigures(int countT, int sizeT, Block *templateT);
+Game *createGame(int width, int height, int count, int size, Block *template);
+Figure *createNewFigure(Game *tetGame);
